ec51e34463bada51fd517d9f0cd9535679c08a05
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2018-10-30 18:07:58 +0100

    Added KotlinTest, Ktlint, Exposed and MockK dependencies (#1)

--- src/main/kotlin/de/unia/se/teamcq/calculator/GreetingController.kt
+++ src/main/kotlin/de/unia/se/teamcq/calculator/GreetingController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
-
 @RestController
 class GreetingController {
 
@@ -14,5 +13,4 @@ class GreetingController {
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
-
 }
