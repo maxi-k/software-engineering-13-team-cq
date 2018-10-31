5e6dae0ff81f9e4b4463a8ea31fc4557ffc605e2
stefan-grafberger <stefangrafberger@gmail.com>
2018-10-31 12:00:45 +0100

    Inital set of examples for MockK usage and DI

--- src/main/kotlin/de/unia/se/teamcq/calculator/EvaluationController.kt
+++ src/main/kotlin/de/unia/se/teamcq/calculator/EvaluationController.kt
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class EvaluationController {
 
+    lateinit var evaluationService: EvaluationService
+
     @CrossOrigin(origins = arrayOf("*"))
     @GetMapping("/eval")
     fun eval(@RequestParam(value = "e") expression: String): EvaluationResult {
         return try {
-            val result = Evaluator.parseAndEvaluate(expression)
+            val result = evaluationService.parseAndEvaluate(expression)
             EvaluationResult("success", expression, result.toString(), null)
         } catch (exception: IllegalArgumentException) {
             exception.printStackTrace()
