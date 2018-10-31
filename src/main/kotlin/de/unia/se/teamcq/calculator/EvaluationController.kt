d995a4033803d4ac6a0a682bd307532adb433b82
stefan-grafberger <stefangrafberger@gmail.com>
2018-10-31 12:47:58 +0100

    fixed error not covered by tests caused by  missing annotation

--- src/main/kotlin/de/unia/se/teamcq/calculator/EvaluationController.kt
+++ src/main/kotlin/de/unia/se/teamcq/calculator/EvaluationController.kt
@@ -1,5 +1,6 @@
 package de.unia.se.teamcq.calculator
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class EvaluationController {
 
+    @Autowired
     lateinit var evaluationService: EvaluationService
 
     @CrossOrigin(origins = arrayOf("*"))
