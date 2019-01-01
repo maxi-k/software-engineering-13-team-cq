2c43235528289d6b32e2303a6e104fc6c03ad221
Fiona Guerin <fiona.guerin@gmx.de>
2019-01-01 16:01:39 +0100

    Added data to notification rule.

--- src/main/kotlin/de/unia/se/teamcq/notification/rule/management/NotificationRule.kt
+++ src/main/kotlin/de/unia/se/teamcq/notification/rule/management/NotificationRule.kt
@@ -1,6 +1,7 @@
 package de.unia.se.teamcq.notification.rule.management
 
 import java.io.Serializable
+import java.util.concurrent.locks.Condition
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -13,5 +14,24 @@ data class NotificationRule (
         val id: Long = 0,
 
         @get: NotNull
-        val name: String
+        val name: String,
+
+        @get: NotNull
+        val recipients: MutableList<String>,
+
+        val description: String,
+
+        //@get: NotNull
+        //val fleets: MutableList<Fleet>,
+
+        //@get: NotNull
+        //val formula: Formula,
+
+        //@get: NotNull
+        //val aggregator: Aggregator,
+
+        //@get: NotNull
+        //val user: User
+
+
 ) : Serializable
