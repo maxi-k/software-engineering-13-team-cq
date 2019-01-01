c185c7d547fc9c556cd638e0db59b4b0cd197f65
Fiona Guerin <fiona.guerin@gmx.de>
2019-01-01 16:48:20 +0100

    Fixing Ktlint problems.

--- src/main/kotlin/de/unia/se/teamcq/notification/rule/management/model/NotificationRule.kt
+++ src/main/kotlin/de/unia/se/teamcq/notification/rule/management/model/NotificationRule.kt
@@ -8,29 +8,28 @@ import javax.persistence.Id
 import javax.validation.constraints.NotNull
 
 @Entity
-data class NotificationRule (
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
-        val id: Long = 0,
+data class NotificationRule(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0,
 
-        @get: NotNull
-        val name: String,
+    @get: NotNull
+    val name: String,
 
-        @get: NotNull
-        val recipients: MutableList<String>,
+    @get: NotNull
+    val recipients: MutableList<String>,
 
-        val description: String
+    val description: String
 
-        //@get: NotNull
-        //val fleets: MutableList<Fleet>,
+    //@get: NotNull
+    // val fleets: MutableList<Fleet>,
 
-        //@get: NotNull
-        //val formula: Formula,
+    // @get: NotNull
+    // val formula: Formula,
 
-        //@get: NotNull
-        //val aggregator: Aggregator,
+    // @get: NotNull
+    // val aggregator: Aggregator,
 
-        //@get: NotNull
-        //val user: User
-
+    // @get: NotNull
+    // val user: User
 
 ) : Serializable
