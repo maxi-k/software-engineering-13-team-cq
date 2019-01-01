5d141ac715b45f94550d9c76c1e1dbd5b6bfc842
Fiona Guerin <fiona.guerin@gmx.de>
2019-01-01 20:14:02 +0100

    Added aggregators.

--- src/main/kotlin/de/unia/se/teamcq/notification/management/CountingAggregator.kt
+++ src/main/kotlin/de/unia/se/teamcq/notification/management/CountingAggregator.kt
@@ -1,8 +1,9 @@
 package de.unia.se.teamcq.notification.management
 
-class ImmediateAggregator: Aggregator {
+class CountingAggregator( val threshold: Int) : Aggregator {
 
     override fun processNotification(notification: NotificationData) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 }
--- src/main/kotlin/de/unia/se/teamcq/notification/management/ScheduledAggregator.kt
+++ src/main/kotlin/de/unia/se/teamcq/notification/management/ScheduledAggregator.kt
@@ -1,6 +1,9 @@
 package de.unia.se.teamcq.notification.management
 
-class ImmediateAggregator: Aggregator {
+import sun.jvm.hotspot.utilities.Interval
+import java.util.concurrent.atomic.AtomicLong
+
+class ScheduledAggregator( val interval: Int ): Aggregator {
 
     override fun processNotification(notification: NotificationData) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
