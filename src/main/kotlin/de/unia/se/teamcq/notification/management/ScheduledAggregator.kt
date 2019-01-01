617aec2b654efb0aba8d76797538caa6e413d566
Fiona Guerin <fiona.guerin@gmx.de>
2019-01-01 20:27:47 +0100

    Fixing Ktlint problems.

--- src/main/kotlin/de/unia/se/teamcq/notification/management/ScheduledAggregator.kt
+++ src/main/kotlin/de/unia/se/teamcq/notification/management/ScheduledAggregator.kt
@@ -1,8 +1,5 @@
 package de.unia.se.teamcq.notification.management
 
-import sun.jvm.hotspot.utilities.Interval
-import java.util.concurrent.atomic.AtomicLong
-
 class ScheduledAggregator(var interval: Int) : Aggregator {
 
     override fun processNotification(notification: NotificationData) {
