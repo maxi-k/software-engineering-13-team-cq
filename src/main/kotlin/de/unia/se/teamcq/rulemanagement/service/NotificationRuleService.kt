8712a63158367a5f87e1e3c39f76d4fbed949009
stefan-grafberger <stefangrafberger@gmail.com>
2019-01-09 16:43:03 +0100

    Fix ktlin error

--- src/main/kotlin/de/unia/se/teamcq/rulemanagement/service/NotificationRuleService.kt
+++ src/main/kotlin/de/unia/se/teamcq/rulemanagement/service/NotificationRuleService.kt
@@ -4,7 +4,6 @@ import de.unia.se.teamcq.rulemanagement.entity.INotificationRuleRepository
 import de.unia.se.teamcq.rulemanagement.model.NotificationRule
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 @Component
 class NotificationRuleService : INotificationRuleService {
