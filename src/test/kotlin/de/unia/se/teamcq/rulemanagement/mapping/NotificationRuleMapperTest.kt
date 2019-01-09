23922a77be39e1539ca4d93a3da63017d354bd28
stefan-grafberger <stefangrafberger@gmail.com>
2019-01-09 18:05:15 +0100

    Fixed Docker Container from MbW and Code Cleanup

--- src/test/kotlin/de/unia/se/teamcq/rulemanagement/mapping/NotificationRuleMapperTest.kt
+++ src/test/kotlin/de/unia/se/teamcq/rulemanagement/mapping/NotificationRuleMapperTest.kt
@@ -3,12 +3,12 @@ package de.unia.se.teamcq.rulemanagement.mapping
 import de.unia.se.teamcq.rulemanagement.dto.NotificationRuleDto
 import de.unia.se.teamcq.rulemanagement.entity.NotificationRuleEntity
 import de.unia.se.teamcq.rulemanagement.model.NotificationRule
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
-import org.assertj.core.api.Assertions.assertThat
 import org.mapstruct.factory.Mappers
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.test.context.ContextConfiguration
-import kotlin.test.assertEquals
 
 @ContextConfiguration(classes = [(TestConfiguration::class)])
 class NotificationRuleMapperTest : StringSpec() {
@@ -18,74 +18,63 @@ class NotificationRuleMapperTest : StringSpec() {
     init {
 
         "convert model to entity" {
-            // Perform a POST request to /events/vehicle-status
-            // given
+
             val notificationRule = NotificationRule(
                     2,
                     "name",
                     "description"
             )
 
-            // when
             val notificationRuleEntity = notificationRuleMapper.modelToEntity(notificationRule)
 
-            // then
-            assertThat(notificationRuleEntity).isNotNull
-            assertEquals(notificationRuleEntity.name, notificationRule.name)
-            assertEquals(notificationRuleEntity.description, notificationRule.description)
+            notificationRuleEntity shouldNotBe null
+            notificationRuleEntity.name shouldBe notificationRule.name
+            notificationRuleEntity.description shouldBe notificationRule.description
         }
 
         "convert entity to model" {
-            // Perform a POST request to /events/vehicle-status
-            // given
+
             val notificationRuleEntity = NotificationRuleEntity(
                     2,
                     "name",
                     "description"
             )
 
-            // when
             val notificationRule = notificationRuleMapper.entityToModel(notificationRuleEntity)
 
-            // then
-            assertThat(notificationRule).isNotNull
-            assertEquals(notificationRuleEntity.name, notificationRule.name)
-            assertEquals(notificationRuleEntity.description, notificationRule.description)
+            notificationRule shouldNotBe null
+            notificationRuleEntity.name shouldBe notificationRule.name
+            notificationRuleEntity.description shouldBe notificationRule.description
         }
 
         "convert model to dto" {
-            // Perform a POST request to /events/vehicle-status
-            // given
+
             val notificationRule = NotificationRule(
                     2,
                     "name",
                     "description"
             )
 
-            // when
             val notificationRuleDto = notificationRuleMapper.modelToDto(notificationRule)
 
-            // then
-            assertThat(notificationRuleDto).isNotNull
-            assertEquals(notificationRuleDto.name, notificationRule.name)
-            assertEquals(notificationRuleDto.description, notificationRule.description)
+            notificationRuleDto shouldNotBe null
+            notificationRuleDto.name shouldBe notificationRule.name
+            notificationRuleDto.description shouldBe notificationRule.description
         }
 
         "convert dto to model" {
-            // given
+
             val notificationRuleDto = NotificationRuleDto(
                     2,
                     "name",
                     "description"
             )
 
-            // when
             val notificationRule = notificationRuleMapper.dtoToModel(notificationRuleDto)
 
-            // then
-            assertThat(notificationRule).isNotNull
-            assertEquals(notificationRuleDto.name, notificationRule.name)
-            assertEquals(notificationRuleDto.description, notificationRule.description)
+            notificationRule shouldNotBe null
+            notificationRuleDto.name shouldBe notificationRule.name
+            notificationRuleDto.description shouldBe notificationRule.description
         }
     }
 }
--- src/test/kotlin/de/unia/se/teamcq/vehiclestate/mapping/VehicleStateMapperTest.kt
+++ src/test/kotlin/de/unia/se/teamcq/vehiclestate/mapping/VehicleStateMapperTest.kt
@@ -2,12 +2,12 @@ package de.unia.se.teamcq.vehiclestate.mapping
 
 import de.unia.se.teamcq.vehiclestate.entity.VehicleStateEntity
 import de.unia.se.teamcq.vehiclestate.model.VehicleState
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
-import org.assertj.core.api.Assertions.assertThat
 import org.mapstruct.factory.Mappers
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.test.context.ContextConfiguration
-import kotlin.test.assertEquals
 
 @ContextConfiguration(classes = [(TestConfiguration::class)])
 class VehicleStateMapperTest : StringSpec() {
@@ -17,8 +17,7 @@ class VehicleStateMapperTest : StringSpec() {
     init {
 
         "convert model to entity" {
-            // Perform a POST request to /events/vehicle-status
-            // given
+
             val vehicleState = VehicleState(
                     2,
                     "name",
@@ -26,19 +25,17 @@ class VehicleStateMapperTest : StringSpec() {
                     0.2
             )
 
-            // when
             val vehicleStateEntity = vehicleStateMapper.modelToEntity(vehicleState)
 
-            // then
-            assertThat(vehicleStateEntity).isNotNull
-            assertEquals(vehicleStateEntity.eventId, vehicleState.eventId)
-            assertEquals(vehicleStateEntity.vehicleId, vehicleState.vehicleId)
-            assertEquals(vehicleStateEntity.kilometers, vehicleState.kilometers)
-            assertEquals(vehicleStateEntity.batteryCharge, vehicleState.batteryCharge)
+            vehicleStateEntity shouldNotBe null
+            vehicleStateEntity.eventId shouldBe vehicleState.eventId
+            vehicleStateEntity.vehicleId shouldBe vehicleState.vehicleId
+            vehicleStateEntity.kilometers shouldBe vehicleState.kilometers
+            vehicleStateEntity.batteryCharge shouldBe vehicleState.batteryCharge
         }
 
         "convert entity to model" {
-            // given
+
             val vehicleStateEntity = VehicleStateEntity(
                     2,
                     "name",
@@ -46,15 +43,13 @@ class VehicleStateMapperTest : StringSpec() {
                     0.2
             )
 
-            // when
             val vehicleState = vehicleStateMapper.entityToModel(vehicleStateEntity)
 
-            // then
-            assertThat(vehicleStateEntity).isNotNull
-            assertEquals(vehicleStateEntity.eventId, vehicleState.eventId)
-            assertEquals(vehicleStateEntity.vehicleId, vehicleState.vehicleId)
-            assertEquals(vehicleStateEntity.kilometers, vehicleState.kilometers)
-            assertEquals(vehicleStateEntity.batteryCharge, vehicleState.batteryCharge)
+            vehicleState shouldNotBe null
+            vehicleStateEntity.eventId shouldBe vehicleState.eventId
+            vehicleStateEntity.vehicleId shouldBe vehicleState.vehicleId
+            vehicleStateEntity.kilometers shouldBe vehicleState.kilometers
+            vehicleStateEntity.batteryCharge shouldBe vehicleState.batteryCharge
         }
     }
 }
