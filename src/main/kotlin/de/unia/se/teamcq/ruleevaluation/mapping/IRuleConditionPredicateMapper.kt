0477f3448c9e4a4bf3b88745a19ca9680c21828f
stefan-grafberger <stefangrafberger@gmail.com>
2019-01-19 00:22:24 +0100

    Minor code cleanup

--- src/main/kotlin/de/unia/se/teamcq/ruleevaluation/mapping/IRuleConditionPredicateMapper.kt
+++ src/main/kotlin/de/unia/se/teamcq/ruleevaluation/mapping/IRuleConditionPredicateMapper.kt
@@ -4,9 +4,8 @@ import de.unia.se.teamcq.ruleevaluation.dto.RuleConditionPredicateDto
 import de.unia.se.teamcq.ruleevaluation.entity.RuleConditionPredicateEntity
 import de.unia.se.teamcq.ruleevaluation.model.RuleConditionPredicate
 import org.mapstruct.Mapper
-import org.mapstruct.ReportingPolicy
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 interface IRuleConditionPredicateMapper {
 
     fun modelToDto(ruleConditionPredicate: RuleConditionPredicate):
