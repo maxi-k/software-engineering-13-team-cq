db338910bb1fb8af52bded27cbb433dbb5ff209d
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-01 02:20:56 +0100

    Remove unnecessary imports

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/controller/PredicateFieldController.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/controller/PredicateFieldController.kt
@@ -1,10 +1,9 @@
 package de.unia.se.teamcq.ruleevaluation.controller
 
-import de.unia.se.teamcq.ruleevaluation.service.IPredicateFieldContainer
 import de.unia.se.teamcq.ruleevaluation.dto.PredicateFieldProviderDto
 import de.unia.se.teamcq.ruleevaluation.mapping.IPredicateFieldProviderMapper
+import de.unia.se.teamcq.ruleevaluation.service.IPredicateFieldContainer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/user/controller/UserController.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/user/controller/UserController.kt
@@ -6,11 +6,11 @@ import de.unia.se.teamcq.user.service.IUserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
