81469c36cd32f1e210763ff3c3b4a9c5af087801
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-14 16:51:25 +0100

    Remove unused test mocks, clean up imports

--- apps/customisable-notification-service-backend/src/test/kotlin/de/unia/se/teamcq/ruleevaluation/service/EvaluationPredicateServiceTest.kt
+++ apps/customisable-notification-service-backend/src/test/kotlin/de/unia/se/teamcq/ruleevaluation/service/EvaluationPredicateServiceTest.kt
@@ -1,25 +1,15 @@
 package de.unia.se.teamcq.ruleevaluation.service
 
 import de.unia.se.teamcq.TestUtils
-import de.unia.se.teamcq.ruleevaluation.model.ComparisonType
-import de.unia.se.teamcq.ruleevaluation.model.PredicateField
-import de.unia.se.teamcq.ruleevaluation.model.RuleConditionPredicate
-import de.unia.se.teamcq.vehiclestate.model.VehicleStateDataTypeBattery
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.mockk.MockKAnnotations
-import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
-import io.mockk.impl.annotations.MockK
-import org.mockito.ArgumentMatchers.any
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.test.context.ContextConfiguration
 
 @ContextConfiguration(classes = [TestConfiguration::class])
 class EvaluationPredicateServiceTest : StringSpec() {
-
-    @MockK
-    lateinit var predicateFieldContainer: IPredicateFieldContainer
 
     @InjectMockKs
     lateinit var evaluationPredicateService: EvaluationPredicateService
@@ -33,12 +23,6 @@ class EvaluationPredicateServiceTest : StringSpec() {
             val vehicleStateDataType = TestUtils.getTestVehicleStateDataTypeBatteryModel()
             val predicate = TestUtils.getTestRuleConditionPredicateModel().apply {
                 comparisonValue = predicateField.fieldValueAccessor(vehicleStateDataType)
-            }
-
-            every {
-                predicateFieldContainer.getPredicateFieldByProviderAndName(any(), any())
-            } returns vehicleStateDataType.predicateFields.find {
-                it.fieldName == predicateField.fieldName
             }
 
             evaluationPredicateService.checkPredicate(
