e171bb6e29a6eb9b07b2d808c1c3a447969f5a30
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-15 23:40:25 +0100

    Test EvaluationPredicateService thoroughly, fix unreachable exception handling code

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/service/EvaluationPredicateService.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/service/EvaluationPredicateService.kt
@@ -6,6 +6,7 @@ import de.unia.se.teamcq.ruleevaluation.model.PredicateField
 import de.unia.se.teamcq.ruleevaluation.model.RuleConditionPredicate
 import de.unia.se.teamcq.vehiclestate.model.VehicleStateDataType
 import org.springframework.stereotype.Component
+import java.lang.ClassCastException
 
 @Component
 class EvaluationPredicateService : IEvaluationPredicateService {
@@ -33,19 +34,23 @@ class EvaluationPredicateService : IEvaluationPredicateService {
         actualVehicleStateValue: T,
         comparisonValue: String
     ): Boolean {
-        val convertedComparisonValue = fieldDataType.convertToFieldType(comparisonValue) as? T
-                ?: throw IllegalArgumentException(
-                        "ComparisonValue Conversion of $comparisonValue to $fieldDataType " +
-                                "failed for comparison $comparisonType.")
-
-        return when (actualVehicleStateValue) {
-            is R -> comparisonType.compare(actualVehicleStateValue, convertedComparisonValue)
-            is S -> comparisonType.compare(actualVehicleStateValue, convertedComparisonValue)
-            else -> throw IllegalArgumentException(
-                    "Given converted comparisonValue $convertedComparisonValue could not be " +
-                            "compared to $actualVehicleStateValue using the comparison $comparisonType. " +
-                            "This can happen if a declared PredicateField DataType mismatches with the " +
-                            "type of the actual backing field.")
+        return try {
+            val convertedComparisonValue = fieldDataType.convertToFieldType(comparisonValue) as T
+            when (actualVehicleStateValue) {
+                is R -> comparisonType.compare(actualVehicleStateValue, convertedComparisonValue)
+                is S -> comparisonType.compare(actualVehicleStateValue, convertedComparisonValue)
+                else -> throw IllegalArgumentException(
+                        "Given converted comparisonValue $convertedComparisonValue could not be " +
+                                "compared to $actualVehicleStateValue using the comparison $comparisonType. " +
+                                "This can happen if a declared PredicateField DataType mismatches with the " +
+                                "type of the actual backing field."
+                )
+            }
+        } catch (classCastException: ClassCastException) {
+            throw IllegalArgumentException(
+                    "ComparisonValue Conversion of $comparisonValue to $fieldDataType " +
+                            "failed for comparison $comparisonType."
+            )
         }
     }
 }
