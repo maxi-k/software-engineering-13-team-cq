8676733f6c3d14d6ce9a6f1e30f7624b6d621d7a
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-15 15:59:49 +0100

    Add null / blank checks to RuleConditionPredicateDTO

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/mapping/IRuleConditionPredicateMapper.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/mapping/IRuleConditionPredicateMapper.kt
@@ -3,10 +3,29 @@ package de.unia.se.teamcq.ruleevaluation.mapping
 import de.unia.se.teamcq.ruleevaluation.dto.RuleConditionPredicateDto
 import de.unia.se.teamcq.ruleevaluation.entity.RuleConditionPredicateEntity
 import de.unia.se.teamcq.ruleevaluation.model.RuleConditionPredicate
+import org.mapstruct.BeforeMapping
 import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring")
 interface IRuleConditionPredicateMapper {
+
+    @BeforeMapping
+    @Throws(IllegalArgumentException::class)
+    fun checkLegalArguments(ruleConditionPredicateDto: RuleConditionPredicateDto) {
+
+        if (ruleConditionPredicateDto.fieldName.isNullOrBlank()) {
+            throw IllegalArgumentException(
+                    "Attribute fieldName of RuleConditionPredicateDto" +
+                    " is required, but was null or a blank string!"
+            )
+        }
+        if (ruleConditionPredicateDto.providerName.isNullOrBlank()) {
+            throw IllegalArgumentException(
+                    "Attribute providerName of RuleConditionPredicateDto" +
+                            " is required, but was null or a blank string!"
+            )
+        }
+    }
 
     fun modelToDto(ruleConditionPredicate: RuleConditionPredicate):
             RuleConditionPredicateDto
