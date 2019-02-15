55f4bd0d3b81e09f6ecaacf5a14ed34b8821be92
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-15 14:02:27 +0100

    Log it when multiple predicatefieldproviders register with the same name

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/service/PredicateFieldContainer.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/ruleevaluation/service/PredicateFieldContainer.kt
@@ -2,6 +2,8 @@ package de.unia.se.teamcq.ruleevaluation.service
 
 import de.unia.se.teamcq.ruleevaluation.model.IPredicateFieldProvider
 import de.unia.se.teamcq.ruleevaluation.model.PredicateField
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -15,15 +17,36 @@ class PredicateFieldContainer : IPredicateFieldContainer {
         return predicateFieldProviders
     }
 
-    override fun getPredicateFieldProviderByName(predicateFieldProviderName: String): IPredicateFieldProvider? =
-            predicateFieldProviders.find { predicateFieldProvider ->
-                predicateFieldProvider.predicateFieldProviderName == predicateFieldProviderName
+    override fun getPredicateFieldProviderByName(predicateFieldProviderName: String): IPredicateFieldProvider? {
+        val foundValues = predicateFieldProviders.filter { predicateFieldProvider ->
+            predicateFieldProvider.predicateFieldProviderName == predicateFieldProviderName
+        }
+        return when {
+            foundValues.isEmpty() -> null
+            foundValues.size > 1 -> {
+                LOGGER.warn("Found multiple PredicateFieldProvider instances with the same name: $predicateFieldProviderName")
+                foundValues[0]
             }
+            else -> foundValues[0]
+        }
+    }
 
     override fun getPredicateFieldByProviderAndName(predicateFieldProviderName: String, predicateFieldName: String): PredicateField? =
             getPredicateFieldProviderByName(predicateFieldProviderName)?.let {
-                it.predicateFields.find { predicateField ->
+                val foundValues = it.predicateFields.filter { predicateField ->
                     predicateField.fieldName == predicateFieldName
                 }
+                return when {
+                    foundValues.isEmpty() -> null
+                    foundValues.size > 1 -> {
+                        LOGGER.warn("Found multiple PredicateField instances with the same name for Provider $predicateFieldProviderName and Field Name $predicateFieldName")
+                        foundValues[0]
+                    }
+                    else -> foundValues[0]
+                }
             }
+
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(PredicateFieldContainer::class.java)
+    }
 }
