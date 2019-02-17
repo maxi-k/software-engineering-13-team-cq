66f3525b456b3e61b948bd21307eebaa826c1c32
stefan-grafberger <stefangrafberger@gmail.com>
2019-02-17 11:44:17 +0100

    Added documentation to ThymeleafConfiguration

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/notificationmanagement/ThymeleafConfiguration.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/notificationmanagement/ThymeleafConfiguration.kt
@@ -1,5 +1,6 @@
 package de.unia.se.teamcq.notificationmanagement
 
+import de.unia.se.teamcq.rulemanagement.model.NotificationRule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
@@ -8,9 +9,21 @@ import org.thymeleaf.spring5.SpringTemplateEngine
 import org.thymeleaf.templatemode.TemplateMode
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 import org.thymeleaf.templateresolver.ITemplateResolver
+import de.unia.se.teamcq.notificationmanagement.service.NotificationTextService
 
+
+/**
+ * A configuration for the [TemplateEngine] used in [NotificationTextService].
+ *
+ * Usually, you don't need additional configuration to use Thymeleaf in Spring Boot
+ * applications, just depending on spring-boot-starter-thymeleaf is enough
+ * thanks to Spring Boot auto configuration. Unfortunately, the [TemplateEngine]
+ * resulting from the auto configuration can only resolve html templates.
+ * To address that, the templateEngine function here provides a named [Bean] that
+ * is able to resolve both html and text templates based on some custom folder structure.
+ */
 @Configuration
-class ThymeleafConfig {
+class ThymeleafConfiguration {
 
     @Bean
     fun notificationsTextSource(): ResourceBundleMessageSource {
