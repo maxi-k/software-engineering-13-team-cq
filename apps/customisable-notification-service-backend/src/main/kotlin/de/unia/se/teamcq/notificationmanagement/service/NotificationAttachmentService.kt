905a67118a4ee9a8b7a8c8c0d9fea3915589fe13
stefan-grafberger <stefangrafberger@gmail.com>
2019-02-18 00:07:24 +0100

    Refactored getCsvAttachment

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/notificationmanagement/service/NotificationAttachmentService.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/notificationmanagement/service/NotificationAttachmentService.kt
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
-import java.io.IOException
-import java.util.Arrays.asList
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVPrinter
 import java.io.BufferedWriter
 import java.io.ByteArrayOutputStream
 import java.io.OutputStreamWriter
+import com.google.common.io.Closeables
+import de.unia.se.teamcq.ruleevaluation.model.IPredicateFieldProvider
+import de.unia.se.teamcq.vehiclestate.model.VehicleState
+import org.slf4j.LoggerFactory
 
 @Component
 class NotificationAttachmentService : INotificationAttachmentService {
@@ -22,72 +24,101 @@ class NotificationAttachmentService : INotificationAttachmentService {
 
     override fun getCsvAttachment(notificationData: NotificationData): Resource {
 
-        val vehicleStateFields = listOf("state_id", "vin")
-        // TODO: Consider using fleetReference.fetchVehicleData to retrieve additional information
+        val dataTypesWithFields = getDataTypesWithFields()
 
-        val vehicleStateDataTypeFields = predicateFieldContainer
-                .getPredicateFieldProviders().map { fieldProvider ->
-                    fieldProvider to fieldProvider.predicateFields.values.map { vehicleStateField ->
-                        vehicleStateField.fieldName!!
-                    }
-                }
-
-        val vehicleStateDataTypeFieldNames = vehicleStateDataTypeFields.flatMap { dataTypeFields ->
-            dataTypeFields.second.map { fieldName ->
-                "${dataTypeFields.first.predicateFieldProviderName}_$fieldName"
-            }
-        }
-
-        val allCsvFieldNames = vehicleStateFields.plus(vehicleStateDataTypeFieldNames)
+        val csvColumnNames = getCsvColumnNames(dataTypesWithFields)
 
         val byteArrayOutputStream = ByteArrayOutputStream()
         var bufferedWriter: BufferedWriter? = null
         var csvPrinter: CSVPrinter? = null
 
+        var threwException = true
+
         try {
+
             bufferedWriter = BufferedWriter(OutputStreamWriter(byteArrayOutputStream))
-            csvPrinter = CSVPrinter(bufferedWriter, CSVFormat.DEFAULT
-                    .withHeader(*allCsvFieldNames.toTypedArray())
-            )
+            val csvFormat = CSVFormat.DEFAULT.withHeader(*csvColumnNames.toTypedArray())
+            csvPrinter = CSVPrinter(bufferedWriter, csvFormat)
 
             notificationData.matchedVehicleStates.map { vehicleState ->
-
-                val vehicleStateFieldValues = asList(
-                        vehicleState.stateId,
-                        vehicleState.vehicleReference!!.vin!!
-                )
-
-                val predicateFieldValues = vehicleStateDataTypeFields.flatMap { dataType ->
-
-                    vehicleState.vehicleStateDataTypes.filter { potentiallyOfTypeDataType ->
-                        potentiallyOfTypeDataType.predicateFieldProviderName == dataType.first.predicateFieldProviderName
-                    }.flatMap { dataTypeInVehicleState ->
-                        dataType.second.map { predicateFieldInDataType ->
-                            dataTypeInVehicleState.retrieveFieldValue(predicateFieldInDataType)
-                        }
-                    }
-                }
-
-                val allRecordValues = vehicleStateFieldValues.plus(predicateFieldValues)
-
-                csvPrinter.printRecord(allRecordValues)
+                val csvRecordValues = getCsvRecordValues(vehicleState, dataTypesWithFields)
+                csvPrinter.printRecord(csvRecordValues)
             }
 
-            println("Write CSV successfully!")
-        } catch (e: Exception) {
-            println("Writing CSV error!")
-            e.printStackTrace()
+            threwException = false
+        } catch (exception: Exception) {
+
+            val ruleId = notificationData.notificationRule.ruleId
+            logger.error("Error while writing Notification Attachment CSV for Rule $ruleId", exception)
         } finally {
-            try {
-                bufferedWriter!!.flush()
-                bufferedWriter.close()
-                csvPrinter!!.close()
-            } catch (iOException: IOException) {
-                println("Flushing/closing error!")
-                iOException.printStackTrace()
-            }
+
+            bufferedWriter!!.flush()
+            Closeables.close(bufferedWriter, threwException)
+            csvPrinter!!.close()
         }
 
         return ByteArrayResource(byteArrayOutputStream.toByteArray())
+    }
+
+    private fun getDataTypesWithFields(): Map<IPredicateFieldProvider, List<String>> {
+
+        val predicateFieldProviders = predicateFieldContainer.getPredicateFieldProviders()
+
+        return predicateFieldProviders.map { fieldProvider ->
+
+            val fieldNamesOfProvider = fieldProvider.predicateFields.values.map { vehicleStateField ->
+                vehicleStateField.fieldName!!
+            }
+            fieldProvider to fieldNamesOfProvider
+        }.toMap()
+    }
+
+    private fun getCsvColumnNames(dataTypesWithFields: Map<IPredicateFieldProvider, List<String>>): List<String> {
+
+        // TODO: Consider using fleetReference.fetchVehicleData to retrieve more information
+        // TODO: than the one below
+        val vehicleStateFields = listOf("state_id", "vin")
+
+        val vehicleStateDataTypeFieldNames = dataTypesWithFields.flatMap { (predicateFieldProvider, fieldNames) ->
+            fieldNames.map { fieldName ->
+                "${predicateFieldProvider.predicateFieldProviderName}_$fieldName"
+            }
+        }
+
+        return vehicleStateFields.plus(vehicleStateDataTypeFieldNames)
+    }
+
+    private fun getCsvRecordValues(
+        vehicleState: VehicleState,
+        dataTypesWithFields: Map<IPredicateFieldProvider, List<String>>
+    ): List<Any?> {
+
+        val vehicleStateFieldValues = listOf(
+                vehicleState.stateId,
+                vehicleState.vehicleReference!!.vin!!
+        )
+
+        val predicateFieldValues = dataTypesWithFields.flatMap { (predicateFieldProvider, fieldNames) ->
+
+            val allVehicleStateDataTypes = vehicleState.vehicleStateDataTypes
+
+            val matchingDataType = allVehicleStateDataTypes.filter { dataType ->
+                dataType.predicateFieldProviderName == predicateFieldProvider.predicateFieldProviderName
+            }
+
+            val fieldValues = matchingDataType.flatMap { dataType ->
+                fieldNames.map { predicateFieldInDataType ->
+                    dataType.retrieveFieldValue(predicateFieldInDataType)
+                }
+            }
+
+            fieldValues
+        }
+
+        return vehicleStateFieldValues.plus(predicateFieldValues)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(NotificationAttachmentService::class.java)
     }
 }
