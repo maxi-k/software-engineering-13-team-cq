3efd6416710731aef6a68d6a122d44009f813424
stefan-grafberger <stefangrafberger@gmail.com>
2019-02-17 01:19:45 +0100

    i18n now working

--- apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/Application.kt
+++ apps/customisable-notification-service-backend/src/main/kotlin/de/unia/se/teamcq/Application.kt
@@ -3,11 +3,26 @@ package de.unia.se.teamcq
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.context.support.ResourceBundleMessageSource
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+
 
 @SpringBootApplication
 @EnableScheduling
-class Application
+class Application : WebMvcConfigurer {
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    fun main(args: Array<String>) {
+        SpringApplication.run(Application::class.java, *args)
+    }
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ResourceBundleMessageSource()
+        messageSource.setBasename("i18n/")
+        messageSource.setDefaultEncoding("UTF-8")
+        return messageSource
+    }
 }
