3702f0753356bfc417b941f63a059a47710a8989
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-22 23:17:05 +0100

    Add switch for adding the owner as an additional recipient

--- apps/customisable-notification-service-frontend/src/modules/rule-modification/parts/RuleModificationGeneral.tsx
+++ apps/customisable-notification-service-frontend/src/modules/rule-modification/parts/RuleModificationGeneral.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
-import { RuleModificationStepView, createInputFieldUpdater } from '../modification-common'
+import {
+  RuleModificationStepView,
+  createInputFieldUpdater,
+  createCheckboxUpdater,
+} from '../modification-common'
+import { FormattedMessage } from 'react-intl'
 
+import Switch from '@material-ui/core/Switch'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import InputField from '@/modules/shared/components/InputField'
 
 const RuleModificationGeneral: RuleModificationStepView = (
@@ -11,13 +18,20 @@ const RuleModificationGeneral: RuleModificationStepView = (
         label="cns.rule.field.name.label"
         value={inProgressRule.name || ""}
         onChange={createInputFieldUpdater(updateField)('name')} />
-
       <InputField
         label="cns.rule.field.description.label"
         multiline
         rows={3}
         value={inProgressRule.description || ""}
         onChange={createInputFieldUpdater(updateField)('description')} />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={inProgressRule.ownerAsAdditionalRecipient}
+            onChange={createCheckboxUpdater(updateField)('ownerAsAdditionalRecipient')}
+            value="ownerAsAdditionalRecipient" />
+        }
+        label={<FormattedMessage id="cns.rule.field.ownerAsAdditionalRecipient.label" />} />
     </div>
   )
 
