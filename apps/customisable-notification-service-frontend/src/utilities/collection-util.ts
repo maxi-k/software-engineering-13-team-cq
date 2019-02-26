b3a4a49690036804f54586b15144496a04cbe70b
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2019-02-26 03:12:28 +0100

    Add link to email validation

--- apps/customisable-notification-service-frontend/src/utilities/collection-util.ts
+++ apps/customisable-notification-service-frontend/src/utilities/collection-util.ts
@@ -118,6 +118,7 @@ export class FieldValidator<ObjectType extends object> {
   )
 
   public static validateEmailAddress = (value: any) => {
+    // From https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
     const mailTestExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     return FieldValidator.validateNonEmptyString(value)
